1521b26efc222aa3178ec174343d0ef3276f0137
hahadeez4-blip <hahadeez4@gmail.com>
2026-04-03 20:13:53 -0400

    Update port.js

--- port.js
+++ port.js
@@ -1,2 +1,4 @@
-const PORT = process.env.PORT || 3000;  // use Render's port
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
